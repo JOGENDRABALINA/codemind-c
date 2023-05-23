b860d5484fb0dd120ca3d87bbb4f033eea90dc51
JOGENDRABALINA <121618316+JOGENDRABALINA@users.noreply.github.com>
2023-05-23 19:37:56 +0530

    #include <stdio.h> int main() { 	int a,b,i; 	scanf("%d%d",&a,&b); 	for(i=a;i<=b;i++) 	{ 		int t=i; 		int f=1; 		while(t) 		{ 			int r=t%10; 			if(r==0||i%r!=0) 			{ 				f=0; 				break; 			} 			t=t/10; 		} 		if(f==1) 		{ 			printf("%d ",i); 		} 	} }

---
+++ Self_Dividing_Numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() 
+{
+	int a,b,i;
+	scanf("%d%d",&a,&b);
+	for(i=a;i<=b;i++)
+	{
+		int t=i;
+		int f=1;
+		while(t)
+		{
+			int r=t%10;
+			if(r==0||i%r!=0)
+			{
+				f=0;
+				break;
+			}
+			t=t/10;
+		}
+		if(f==1)
+		{
+			printf("%d ",i);
+		}
+	}
+}
